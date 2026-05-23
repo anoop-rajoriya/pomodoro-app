04fedac35c524bf3f072952652c31357e25464ae
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2026-05-23 13:35:20 +0530

    Refactor imports in Timer and Stopwatch components to remove React import

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { usePreferences } from "./PreferencesContext";
 
 const formatTime = (ms: number) => {
